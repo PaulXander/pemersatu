b0fc0c9bc386ad53023f70f37a04283ab77f15c8
PaulXander <88633204+PaulXander@users.noreply.github.com>
2021-08-10 21:06:15 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,4 +50,4 @@ class Bot(Client):
 
     async def stop(self, *args):
         await super().stop()
-        self.LOGGER(__name__).info("Bot Running.")
+        self.LOGGER(__name__).info("Bot Stopped.")
